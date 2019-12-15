556af93a8f3f1420b9051a3e1ffa0cfb91c94bde
Jasper <well.caffeinated@gmail.com>
2019-12-15 13:55:05 -0700

    Create v3-fog.js

---
+++ src/components/three-vue/v3-fog.js
@@ -0,0 +1,32 @@
+import * as THREE from 'three'
+import THREEObjectMixin from '@/components/three-vue/v3-object.mixin'
+
+const threeProps = {
+  color: {
+    type: Number
+    , default: 0x000000
+  }
+  , near: {
+    type: Number
+    , default: 1
+  }
+  , far: {
+    type: Number
+    , default: 1000
+  }
+}
+
+export default {
+  name: 'v3-fog'
+  , inject: [ 'threeVue' ]
+  , props: {
+    ...threeProps
+  }
+  , beforeDestroy(){
+    this.threeVue.scene.fog = null
+  }
+  , render(h){
+    this.threeVue.scene.fog = new THREE.Fog( this.color, this.near, this.far )
+    return h('div')
+  }
+}
